0b314f0b593710ddf8f4ad7d95fb1e4f8297f68b
gorilla <savonovv73@gmail.com>
2026-05-02 09:00:00 +0400

    errors1-5: implement custom error types and From traits

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -15,11 +15,10 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
 
 // TODO: Fix the compiler error by changing the signature and body of the
 // `main` function.
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    // Don't change this line.
     let cost = total_cost(pretend_user_input)?;
 
     if cost > tokens {
@@ -28,4 +27,5 @@ fn main() {
         tokens -= cost;
         println!("You now have {tokens} tokens.");
     }
+    Ok(())
 }
